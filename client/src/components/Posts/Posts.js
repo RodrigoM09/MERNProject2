58644ce4bb8db509dec6db279b9c1ce0e80d00d6
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-02 15:04:31 -0600

    autosave script

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,26 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
 
 import Post from "./Post/Post";
-
-// import useStyles from "../Form/styles";
+import useStyles from "./styles";
 
 //this component will render the Post component, which will render the individual posts.
 //the Posts component will render the Post component, which will render the individual posts.
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     //this is a hook that allows us to access the posts in the store, which will be rendered by the Post component
     const posts = useSelector((state) => state.posts);
-    // const classes = useStyles();
+    const classes = useStyles();
 
     console.log(posts);
 
     return (
-        <>
-        <h1>Form</h1>
-        <Post />
-        <Post />
-        </>
-    );
+       !posts.length ? <CircularProgress/> : (
+           <Grid className={classes.container} container alignItems={"stretch"} spacing={3}>
+               {posts.map((post) => (
+                   <Grid key={post._id} item xs={12} sm={6}>
+                          <Post post={post} setCurrentId={setCurrentId}/>
+                     </Grid>
+               ))}
+           </Grid>
+    ));
 }
 
 export default Posts;
