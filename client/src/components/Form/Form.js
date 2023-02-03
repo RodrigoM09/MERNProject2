241fefcd289299367e681ebf8680a72faa1871e1
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-03 10:45:58 -0600

    autosave script

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
 import {useDispatch, useSelector} from "react-redux";
@@ -15,28 +15,38 @@ import { createPost, updatePost } from "../../actions/posts";
 // FileBase is a component that allows us to upload a file, and it will return a base64 string, which we can store in the state
 const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: ''});
-
     const posts = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(createPost(postData));
-        if (currentId) {
-            dispatch(updatePost(currentId, postData));
-        } else {
-            dispatch(createPost(postData));
-        }
-    }
-    const clear = () => {
+    useEffect(() => {
+        if (posts) setPostData(posts);
+    }, [posts]);
 
+
+
+    const clear = () => {
+        setCurrentId(0);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+
+        if (currentId === 0) {
+            dispatch(createPost(postData));
+            clear();
+        } else {
+            dispatch(updatePost(currentId, postData));
+            clear();
+        }
+    };
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Creating a Memory</Typography>
+                <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
@@ -48,6 +58,7 @@ const Form = ({ currentId, setCurrentId }) => {
         </Paper>
 
     );
-}
+};
+
 
 export default Form;
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getPosts, createPosts, updatePost, deletePost, likePost } from "../controllers/posts.js";
+import { getPosts, createPosts, updatePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -9,8 +9,8 @@ const router = express.Router();
 router.get("/", getPosts);
 router.post("/", createPosts);
 router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+// router.delete("/:id", deletePost);
+// router.patch("/:id/likePost", likePost);
 
 
 export default router;
